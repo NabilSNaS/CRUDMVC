352b92e1cd83fb3f47752bb5abc7c5766ccfd6cb
Nabila <hafija.pstu.cse@gmail.com>
2022-07-07 23:32:05 +0600

    first commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDMVC.Startup))]
+namespace CRUDMVC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
